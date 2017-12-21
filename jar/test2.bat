ad0816c0b69da2e4c252fafc860dca011f6a04ba
todd <masont@bigpond.com>
2017-12-21 23:54:18 +1100

    Changes to be committed: 	modified:   combat.doc 	deleted:    jar/test2.bat 	new file:   jar/test_move.bat 	renamed:    tests/test_move.txt -> jar/tests/test_move.txt

--- jar/test2.bat
+++
@@ -1,2 +0,0 @@
-::java -cp combat.jar tests.TestEventMoveGUI --load ../tests/test_move.txt >test.log
-java -cp combat.jar tests.TestEventMove --load ../tests/test_move.txt >test.log
---
+++ jar/test_move.bat
@@ -0,0 +1,4 @@
+::java -cp combat.jar tests.TestEventMoveGUI --load tests/test_move.txt >test.log
+java -cp combat.jar tests.TestEventMove --load tests/test_move.txt 
+pause
+
